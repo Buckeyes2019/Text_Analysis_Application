6062f6f819024847f7516d101f14c85ae46265f2
Buckeyes2019 <55885798+Buckeyes2019@users.noreply.github.com>
2022-05-19 06:21:49 -0400

    Update Conduit_Application.py

--- Conduit_Application.py
+++ Conduit_Application.py
@@ -190,7 +190,7 @@ if selected == 'Named Entity Recognition':
                 df14 = df14[df14['Type of Entity'].isin(options1)]
                 df14['Type of Entity'] = df14['Type of Entity'].str.replace('PERSON', 'Person').str.replace('ORG', 'Organization').str.replace('LOC', 'Location').str.replace('GPE', 'Geopolitical Entity')
                 df14.sort_values(['Type of Entity'], ascending=False, inplace=True)
-                df14.groupby(["Type of Entity", "Entity"]).size().reset_index(name="Count").sort_values(by=["Type of Entity", "Count"], ascending=False, inplace=True)
+                df14 = df14.groupby(["Type of Entity", "Entity"]).size().reset_index(name="Count").sort_values(by=["Type of Entity", "Count"], ascending=False, inplace=True)
 		
                 st.write(df14)
                 NER_Results1 = df14.to_csv(index=False, header=True)
